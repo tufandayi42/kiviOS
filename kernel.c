02767bb7cdaf5ecdbe4ce8ca5d68e5e8d689fa27
Mustafa Kavak <101901319+mustafa0kavak@users.noreply.github.com>
2024-03-22 22:07:08 +0300

    Kernel.c osurukçu dorukay update

--- kernel.c
+++ kernel.c
@@ -2,30 +2,25 @@
 #include "./include/c++/13.2.0/iostream"
 
 void kernel_main() {
-    clear_screen();
-    print_msg("Dorukaya, sevgili dostum,
-               Kivilerden sana selam olsun.
-               Uzaklardan sesleniyorum sana,
-               Bu mektup taştan kalbimden.
-
-               Bilmelisin ki seni hiç unutmadım,
-               Sohbetlerimizi, kahkahalarımızı.
-               Birlikte yediğimiz kivileri,
-               Tatlarını, kokularını.
-
-               Burada, taşların arasında,
-               Seni özlüyorum her gün biraz daha.
-               Gözlerimi kapattığımda,
-               Seni karşımda görüyorum, gülümserken.
-
-               Dorukaya, sevgili dostum,
-               Kivilerden sana bir Chris sözüm var:
-               Bir gün yine bir araya geleceğiz,
-               Ve birlikte kiviler yiyeceğiz.
-
-               Taştan kalbim sana selam yolluyor,
-               Ve sana olan sevgisini fısıldıyor.
-               Unutma ki seni seviyorum,
-               Ve her zaman seveceğim.\n");
-    
+  clear_screen();
+  print_msg("Dorukaya, sevgili dostum,\n"
+            "Kivilerden sana selam olsun.\n"
+            "Uzaklardan sesleniyorum sana,\n"
+            "Bu mektup taştan kalbimden.\n\n"
+            "Bilmelisin ki seni hiç unutmadım,\n"
+            "Sohbetlerimizi, kahkahalarımızı.\n"
+            "Birlikte yediğimiz kivileri,\n"
+            "Tatlarını, kokularını.\n\n"
+            "Burada, taşların arasında,\n"
+            "Seni özlüyorum her gün biraz daha.\n"
+            "Gözlerimi kapattığımda,\n"
+            "Seni karşımda görüyorum, gülümserken.\n\n"
+            "Dorukaya, sevgili dostum,\n"
+            "Kivilerden sana bir **Doruk** sözüm var:\n"
+            "Bir gün yine bir araya geleceğiz,\n"
+            "Ve birlikte kiviler yiyeceğiz.\n\n"
+            "Taştan kalbim sana selam yolluyor,\n"
+            "Ve sana olan sevgisini fısıldıyor.\n"
+            "Unutma ki seni seviyorum,\n"
+            "Ve her zaman seveceğim(angry birds).\n");
 }
