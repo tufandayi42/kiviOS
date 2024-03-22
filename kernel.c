bbd1c150c3e4b23d7d24d51495cdf928fbd74ede
Mustafa Kavak <101901319+mustafa0kavak@users.noreply.github.com>
2024-03-22 21:59:54 +0300

    DORUKAYA KİVİ YEDİR

--- kernel.c
+++ kernel.c
@@ -1,6 +1,31 @@
 #include "./include/print.h"
+#include "./include/c++/13.2.0/iostream"
 
 void kernel_main() {
     clear_screen();
-    print_msg("womp womp bitter cikolata\n");
+    print_msg("Dorukaya, sevgili dostum,
+               Kivilerden sana selam olsun.
+               Uzaklardan sesleniyorum sana,
+               Bu mektup taştan kalbimden.
+
+               Bilmelisin ki seni hiç unutmadım,
+               Sohbetlerimizi, kahkahalarımızı.
+               Birlikte yediğimiz kivileri,
+               Tatlarını, kokularını.
+
+               Burada, taşların arasında,
+               Seni özlüyorum her gün biraz daha.
+               Gözlerimi kapattığımda,
+               Seni karşımda görüyorum, gülümserken.
+
+               Dorukaya, sevgili dostum,
+               Kivilerden sana bir Chris sözüm var:
+               Bir gün yine bir araya geleceğiz,
+               Ve birlikte kiviler yiyeceğiz.
+
+               Taştan kalbim sana selam yolluyor,
+               Ve sana olan sevgisini fısıldıyor.
+               Unutma ki seni seviyorum,
+               Ve her zaman seveceğim.\n");
+    
 }
