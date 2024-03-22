c0df0419b34badaa1d081338e211b46c24385583
tufandayi42 <arikkoqp@gmail.com>
2024-03-22 22:23:40 +0300

    Negro -> Nero

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Welcome to KiviOs!
 
 We are a small group of two and we are making KiviOs! 
+I just have drank 2 Liters of Nihat Hoca Parfüm!
--- kernel.c
+++ kernel.c
@@ -2,29 +2,29 @@
 
 void kernel_main() {
     clear_screen();
-    print_msg("Dorukaya, sevgili dostum,
-               Kivilerden sana selam olsun.
-               Uzaklardan sesleniyorum sana,
-               Bu mektup taştan kalbimden.
+    print_msg("Dorukaya, sevgili dostum,\n");
+    print_msg("Kivilerden sana selam olsun.\n");
+    print_msg("Uzaklardan sesleniyorum sana,\n");
+    print_msg("Bu mektup taştan kalbimden.\n");
 
-               Bilmelisin ki seni hiç unutmadım,
-               Sohbetlerimizi, kahkahalarımızı.
-               Birlikte yediğimiz kivileri,
-               Tatlarını, kokularını.
+    print_msg("Bilmelisin ki seni hiç unutmadım,\n");
+    print_msg("Sohbetlerimizi, kahkahalarımızı.\n");
+    print_msg("Birlikte yediğimiz kivileri,\n");
+    print_msg("Tatlarını, kokularını.\n\n");
 
-               Burada, taşların arasında,
-               Seni özlüyorum her gün biraz daha.
-               Gözlerimi kapattığımda,
-               Seni karşımda görüyorum, gülümserken.
+    print_msg("Burada, taşların arasında,\n");
+    print_msg("Seni özlüyorum her gün biraz daha.\n");
+    print_msg("Gözlerimi kapattığımda,\n");
+    print_msg("Seni karşımda görüyorum, gülümserken.\n\n");
 
-               Dorukaya, sevgili dostum,
-               Kivilerden sana bir Chris sözüm var:
-               Bir gün yine bir araya geleceğiz,
-               Ve birlikte kiviler yiyeceğiz.
+    print_msg("Dorukaya, sevgili dostum,\n");
+    print_msg("Kivilerden sana bir Chris sözüm var:\n");
+    print_msg("Bir gün yine bir araya geleceğiz,\n");
+    print_msg("Ve birlikte kiviler yiyeceğiz.\n\n");
 
-               Taştan kalbim sana selam yolluyor,
-               Ve sana olan sevgisini fısıldıyor.
-               Unutma ki seni seviyorum,
-               Ve her zaman seveceğim.\n");
+    print_msg("Taştan kalbim sana selam yolluyor,\n");
+    print_msg("Ve sana olan sevgisini fısıldıyor.\n");
+    print_msg("Unutma ki seni seviyorum,\n");
+    print_msg("Ve her zaman seveceğim.\n");
     
 }
